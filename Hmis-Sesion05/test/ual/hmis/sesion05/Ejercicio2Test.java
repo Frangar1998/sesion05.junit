9a0433b1d1b079b4a04668d0ca6f8e49160a3d09
Francisco <fgl244@inlumine.ual.es>
2019-05-27 16:30:09 +0200

    Ejercicios 3 y 4

--- Hmis-Sesion05/test/ual/hmis/sesion05/Ejercicio2Test.java
+++ Hmis-Sesion05/test/ual/hmis/sesion05/Ejercicio2Test.java
@@ -2,7 +2,6 @@ package ual.hmis.sesion05;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
